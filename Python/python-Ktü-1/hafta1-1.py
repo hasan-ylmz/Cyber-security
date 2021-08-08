8e58fe48359b6cdc47fe9150f96dfa8cc86e0521
Hasan-ylmz <hasanktu98@outlook.com>
2021-08-08 22:26:48 +0300

    delete ktü

--- Python/python-Ktü-1/hafta1-1.py
+++
@@ -1,7 +0,0 @@
-year=int(input("Doğum tarihini giriniz = "))
-a=(2021-year)
-
-if a>=18:
-    print("Ehliyet alabilir")
-else:
-    print("Ehliyet alamaz") 
--- Python/python-Ktü-1/hafta1-2.py
+++
@@ -1,8 +0,0 @@
-price=int(input("Ürün fiyatını  giriniz = "))
-borc=0
-if price>100:
-    borc=price
-    print("borcunuz ", borc)
-else:
-    borc=price+10
-    print("borcunuz ",borc)
